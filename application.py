68e3bec039375be6b7a515797c1d555bd7ef500c
Max <editing4life@gmail.com>
2023-05-31 10:46:21 +1200

    small changes

--- application.py
+++ application.py
@@ -1,34 +1,119 @@
-import time
 from typing import List
 
-from plugins.croesus.croesus_helper import CroesusHelper
-from plugins.plugin import Plugin
+from PyQt6.QtWidgets import QMainWindow, QPushButton, QVBoxLayout, QWidget, QListWidget, QListWidgetItem, QCheckBox, \
+    QApplication
+from PyQt6.QtCore import Qt, QThread
+from rs3helper.plugins.croesus.croesus_helper import CroesusHelper
+from rs3helper.plugins.plugin import Plugin
+import time
+import traceback
+import sys
 
 
-
-class Application:
+class Application(QThread):
     def __init__(self):
+        super().__init__()
         self.plugins: List[Plugin] = []
         self.running: bool = False
 
     def load_plugin(self, plugin: Plugin):
-        self.plugins.append(plugin)
+        try:
+            self.plugins.append(plugin)
+            plugin.disable()  # Start with plugin disabled
+            print(f"Loaded plugin: {plugin.__class__.__name__}")
+        except Exception as e:
+            print(f"Error occurred while loading plugin: {str(e)}")
 
-    def start(self):
+    def run(self):
         self.running = True
+        print("Starting application...")
         while self.running:
             for plugin in self.plugins:
+                print("Checking if plugin is enabled: " + str(plugin.enabled))
                 if plugin.enabled:
-                    print('Updating')
-                    plugin.update()
+                    try:
+                        print(f"Updating plugin: {plugin.__class__.__name__}")
+                        plugin.update()
+                    except Exception as e:
+                        print(f"Exception occurred: {e}")
+                        traceback.print_exc()
             time.sleep(0.1)
 
-    def stop(self):
-        self.running = False
+
+class ApplicationGUI(QMainWindow):
+    def __init__(self, application, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.setWindowTitle("Rs3Helper")
+        self.setWindowFlag(Qt.WindowType.WindowStaysOnTopHint, True)
+
+        self.application = application
+
+        # Set up the main widget and layout
+        self.main_widget = QWidget(self)
+        self.setCentralWidget(self.main_widget)
+
+        self.layout = QVBoxLayout(self.main_widget)
+
+        # Set up the start/stop button
+        self.start_stop_button = QPushButton("Start", self)
+        self.start_stop_button.clicked.connect(self.toggle_application)
+        self.layout.addWidget(self.start_stop_button)
+
+        # Set up the list of plugins
+        self.plugin_list = QListWidget(self)
+        self.layout.addWidget(self.plugin_list)
+
+        # Load the plugins
+        self.load_plugins()
+
+        self.plugin_list.itemChanged.connect(self.plugin_item_changed)
+
+    def toggle_application(self):
+        if self.application.isRunning():
+            self.application.running = False
+            self.start_stop_button.setText("Start")
+            for plugin in self.application.plugins:
+                plugin.gui.hide()  # Hide the GUI of each plugin when the application stops
+        else:
+            self.application.start()
+            self.start_stop_button.setText("Stop")
+            for plugin in self.application.plugins:
+                if plugin.enabled:
+                    plugin.gui.show()  # Show the GUI of each enabled plugin when the application starts
+
+    def load_plugins(self):
+        for plugin in self.application.plugins:
+            # Create a list item for each plugin
+            item = QListWidgetItem(f"{plugin.__class__.__name__}")
+            item.setCheckState(Qt.CheckState.Checked if plugin.enabled else Qt.CheckState.Unchecked)
+            self.plugin_list.addItem(item)
+
+    def plugin_item_changed(self, item):
+        for plugin in self.application.plugins:
+            if f"{plugin.__class__.__name__}" == item.text():
+                if item.checkState() == Qt.CheckState.Checked:
+                    plugin.enable()
+                    if self.application.isRunning():
+                        plugin.gui.show()
+                else:
+                    plugin.disable()
+                    plugin.gui.hide()
 
 
 if __name__ == '__main__':
-    app = Application()
-    croesus_helper = CroesusHelper()
-    app.load_plugin(croesus_helper)
-    app.start()
+    try:
+        app = QApplication(sys.argv)
+
+        application = Application()
+        croesus_helper = CroesusHelper()
+        application.load_plugin(croesus_helper)  # Here
+
+        gui = ApplicationGUI(application)
+        gui.show()
+
+        sys.exit(app.exec())
+
+    except Exception as e:
+        print(f"Error occurred while running application: {str(e)}")
+        traceback.print_exc()
